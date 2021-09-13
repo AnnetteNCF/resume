2b5b78e18a8f2ca5416788d638d78d952bc67108
annette <annette@champria.gg>
2021-09-13 00:41:38 -0700

    changed menu color, added border in hero text

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 
   const contactContainer = document.querySelector('.contact-information');
+  const menuContainer = document.querySelector('.menu-container');
+  const topLine = document.querySelector('#line-one');
 
-//   const contactDisplay = () => {
-//       contactBtn.classList.toggle('active');
-//       console.log('I AM A BUTTON!!!!')
-//   }
-
-//   contactBtn.addEventListener('click', contactDisplay);
 
 function contactDisplay() {
     contactContainer.classList.toggle('active');
     console.log('I AM A BUTTON!');
+}
+
+function menuDisplay() {
+  menuContainer.classList.toggle('is-active');
+  topLine.classList.toggle('rotate-top');
+  console.log('I AM A MENU BUTTON!');
 }
