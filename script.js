a6608e74fc987df6b318c0279de7fb453dd311b3
annette <annette@champria.gg>
2021-09-17 23:39:15 -0700

    fixed hero and project section responsiveness

--- script.js
+++ script.js
@@ -1,28 +1,8 @@
 
-  const contactContainer = document.querySelector('.contact-information');
-  const menuContainer = document.querySelector('.menu-container');
-  // const topLine = document.querySelector('#line-one');
-  const mouseCursor= document.querySelector('#cursor-container');
-
+const contactContainer = document.querySelector('.contact-information');
 
 function contactDisplay() {
     contactContainer.classList.toggle('active');
     console.log('I AM A BUTTON!');
 }
 
-function menuDisplay() {
-  menuContainer.classList.toggle('is-active');
-  topLine.classList.toggle('rotate-top');
-  console.log('I AM A MENU BUTTON!');
-}
-
-window.addEventListener("mousemove", cursor)
-
-function cursor(e) {
-  mouseCursor.style.top = e.pageY + "px";
-  mouseCursor.style.left = e.pageX + "px"
-}
-
-// window.addEventListener("mousedown", () => {
-//   mouseCursor.classList.add("mouse-down")
-// })
